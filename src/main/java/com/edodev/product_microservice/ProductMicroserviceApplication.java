e8faa2c1676fe4faad413246183fdf0ab93f8b17
Eduardo Bravo <eduardobravod@gmail.com>
2024-06-18 14:55:26 -0400

    Add eureka configuration

--- src/main/java/com/edodev/product_microservice/ProductMicroserviceApplication.java
+++ src/main/java/com/edodev/product_microservice/ProductMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.edodev.product_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductMicroserviceApplication {
 
 	public static void main(String[] args) {
